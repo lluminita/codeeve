e6a01576bc21f0ef344d5f835977ee45218d3136
zampeta <zampeta@gmail.com>
2015-08-26 21:23:08 +0200

    Changed the coach attributes, added the title of the project

--- projects/models.py
+++ projects/models.py
@@ -5,9 +5,16 @@ from django.forms.models import ChoiceField
 
 
 class Project(models.Model):
+    # Project attributes
     title = models.CharField(max_length=200)
-    owner = models.ForeignKey('auth.User', related_name='owns_project')
     description = models.TextField()
     max_members = models.IntegerField()
-    participants = models.ManyToManyField('auth.User', related_name='participant_of_projects')
     difficulty = ChoiceField(choices=['beginner', 'intermediate', 'advanced'])
+    # Project, user attributes
+    owner = models.ForeignKey('auth.User', related_name='owns_project')
+    participants = models.ManyToManyField('auth.User', related_name='participant_of_projects',
+                                          blank=True)
+    coach = models.ForeignKey('auth.User', related_name='coach_of_projects', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
