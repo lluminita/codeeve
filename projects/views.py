79ec35780694d17182743bc374f2b48e4aeba291
Luminita Lazarescu <luminita.lazarescu@deliveryhero.com>
2015-09-09 19:35:25 +0200

    Remove the get_context_data

--- projects/views.py
+++ projects/views.py
@@ -5,7 +5,3 @@ from django.views.generic.list import ListView
 
 class ProjectListView(ListView):
     model = Project
-
-    def get_context_data(self, **kwargs):
-        context = super(ProjectListView, self).get_context_data(**kwargs)
-        return context
