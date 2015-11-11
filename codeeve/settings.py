2c3a407ab2e971d14675c835297f9cfbcbb659e1
Luminita-Cristina Lazarescu <luminita-cristina.lazarescu@deliveryhero.com>
2015-11-11 19:21:28 +0100

    Revert "WIP login"

--- codeeve/settings.py
+++ codeeve/settings.py
@@ -37,11 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account', #??
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.github',
     'codeeve',
     'projects',
 )
@@ -88,13 +83,6 @@ DATABASES = {
     }
 }
 
-AUTHENTICATION_BACKENDS = (
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-    # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
-)
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -115,5 +103,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-SITE_ID = 1
--- codeeve/urls.py
+++ codeeve/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
@@ -9,9 +9,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^$', TemplateView.as_view(template_name='codeeve/index.html'), name='home'),
-    url(r'^projects/', include('projects.urls')),
-    url(r'^accounts/profile/$', RedirectView.as_view(pattern_name='home')),
-    url(r'^accounts/profile/$', RedirectView.as_view(pattern_name='home')),
+    url(r'^$', TemplateView.as_view(template_name='codeeve/index.html')),
+    url(r'^projects/', include('projects.urls'))
 ]
