5fd0e886447e56a0be4354b636daee55d3a08fb5
luminita <cluminita.lazarescu@gmail.com>
2016-03-09 09:51:41 +0100

    add update and details features

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from .models import Project
 
@@ -7,20 +8,22 @@ from .models import Project
 class ProjectCreate(CreateView):
     template_name = 'projects/create_project.html'
     model = Project
-    fields = '__all__'
+    fields = ['title', 'description', 'max_members', 'difficulty',
+              'image', 'participants', 'coach']
     success_url = reverse_lazy('project_list')
 
 
 class ProjectUpdate(UpdateView):
+    template_name = 'projects/update_project.html'
     model = Project
-    fields = '__all__'
-    success_url = reverse_lazy('project_list')
-
-
-class ProjectDelete(DeleteView):
-    model = Project
-    success_url = reverse_lazy('project_list')
+    fields = ['title', 'description', 'max_members', 'difficulty',
+              'image', 'participants', 'coach']
+    success_url = reverse_lazy('project_details')
 
 
 class ProjectListView(ListView):
+    model = Project
+
+
+class ProjectDetailView(DetailView):
     model = Project
